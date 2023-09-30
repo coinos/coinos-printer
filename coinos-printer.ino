e6b29fdc88c2d652646683476ea2aa766b8f1070
Adam Soltys <asoltys@gmail.com>
2023-09-29 22:43:47 -0700

    remote updates

--- coinos-printer.ino
+++ coinos-printer.ino
@@ -1,7 +1,9 @@
 #include "defines.h"
 #include <ArduinoOTA.h>
+#include <HTTPClient.h>
 #include <PubSubClient.h>
 #include <TimeLib.h>
+#include <Update.h>
 #include <WiFi.h>
 
 #define CHECKSUM_SIZE 4
@@ -12,6 +14,9 @@ HardwareSerial printer(1);
 WiFiClient espClient;
 PubSubClient mqtt(espClient);
 String paymentHash;
+String clientId;
+
+const char *url = "https://coinos.io/api/public/coinos-printer.ino.bin";
 
 void split(String message, char delimiter, String parts[], int partsSize) {
   int partIndex = 0;
@@ -37,6 +42,53 @@ void callback(char *topic, byte *payload, unsigned int length) {
   char message[length + 1];
   memcpy(message, payload, length);
   message[length] = '\0';
+
+  if (strncmp(message, "update:", 7) == 0) {
+    HTTPClient http;
+
+    http.begin(url);
+
+    Serial.println("HTTP request started");
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK) {
+        int contentLength = http.getSize();
+
+        bool canBegin = Update.begin(contentLength);
+        if (canBegin) {
+          Serial.println("Begin OTA. This may take 2 - 5 minutes to complete.");
+          size_t written = Update.writeStream(http.getStream());
+
+          if (written == contentLength) {
+            Serial.println("Written : " + String(written) + " successfully");
+          } else {
+            Serial.println("Written only : " + String(written) + "/" +
+                           String(contentLength) + ". Retry?");
+          }
+
+          if (Update.end()) {
+            Serial.println("OTA done!");
+            if (Update.isFinished()) {
+              Serial.println("Update successfully completed. Rebooting.");
+              ESP.restart();
+            } else {
+              Serial.println("Update not finished? Something went wrong!");
+            }
+          } else {
+            Serial.println("Error Occurred. Error #: " +
+                           String(Update.getError()));
+          }
+
+        } else {
+          Serial.println("Not enough space to begin OTA");
+          http.end();
+        }
+      }
+    } else {
+      Serial.println("HTTP error: " + http.errorToString(httpCode));
+    }
+    http.end();
+  }
 
   if (strncmp(message, "pay:", 4) == 0) {
     memmove(message, message + 4, strlen(message + 4) + 1);
@@ -121,6 +173,8 @@ void setup() {
   Serial.begin(115200);
   printer.begin(9600, SERIAL_8N1, -1, 6);
 
+  clientId = "Client-" + String(random(0xffff), HEX);
+
   while (!Serial)
     delay(10);
 
@@ -136,9 +190,6 @@ void setup() {
 void reconnect() {
   while (!mqtt.connected()) {
     Serial.print("Attempting MQTT connection...");
-
-    String clientId = "Client-";
-    clientId += String(random(0xffff), HEX);
 
     if (mqtt.connect(clientId.c_str(), "admin", "MPJzfq97")) {
       Serial.println("connected");
